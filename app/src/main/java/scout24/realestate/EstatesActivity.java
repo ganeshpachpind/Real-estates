c3373d1ba28947432aba6e50e486544f7e0deca0
ganeshp <ganesh_pachpind@external.mckinsey.com>
2017-07-01 19:09:14 +0530

    Renamed MainActivity to EstatesActivity

--- app/src/main/java/scout24/realestate/EstatesActivity.java
+++ app/src/main/java/scout24/realestate/EstatesActivity.java
@@ -3,11 +3,11 @@ package scout24.realestate;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class EstatesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_estates);
     }
 }
